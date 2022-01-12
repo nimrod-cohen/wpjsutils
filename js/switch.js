1c2e6bad7d6f99dfb51a6156b5bd703b352666ac
nimrod-cohen <nimrod.cohen@gmail.com>
2022-01-12 16:35:08 +0200

    add switch control

---
+++ js/switch.js
@@ -0,0 +1,12 @@
+JSUtils.domReady(() => {
+  let inputs = document.querySelectorAll(".wpjsutils.switch");
+  inputs.forEach((input) => {
+    const round = input.classList.contains("round");
+    const wrapper = document.createElement("label");
+    wrapper.classList.add("switch-wrapper");
+    input.parentNode.insertBefore(wrapper, input);
+
+    wrapper.appendChild(input);
+    wrapper.insertAdjacentHTML("beforeend", `<span class="slider ${round ? "round" : ""}"></span>`);
+  });
+});

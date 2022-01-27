14ca6461121259f41520b3395a55d71ea3e62ed9
nimrod-cohen <nimrod.cohen@gmail.com>
2022-01-27 17:52:47 +0200

    allow dynamic switch attaching (after page load)

--- js/switch.js
+++ js/switch.js
@@ -1,12 +1,14 @@
 JSUtils.domReady(() => {
-  let inputs = document.querySelectorAll(".wpjsutils.switch");
-  inputs.forEach((input) => {
-    const round = input.classList.contains("round");
-    const wrapper = document.createElement("label");
-    wrapper.classList.add("switch-wrapper");
+  JSUtils.switch = input => {
+    const round = input.classList.contains('round');
+    const wrapper = document.createElement('label');
+    wrapper.classList.add('switch-wrapper');
     input.parentNode.insertBefore(wrapper, input);
 
     wrapper.appendChild(input);
-    wrapper.insertAdjacentHTML("beforeend", `<span class="slider ${round ? "round" : ""}"></span>`);
-  });
+    wrapper.insertAdjacentHTML('beforeend', `<span class="slider ${round ? 'round' : ''}"></span>`);
+  };
+
+  let inputs = document.querySelectorAll('.wpjsutils.switch');
+  inputs.forEach(input => JSUtils.switch(input));
 });
--- wpjsutils.php
+++ wpjsutils.php
@@ -3,7 +3,7 @@
 * Plugin Name: WPJSUtils
 * Plugin URI: https://google.com?q=who+is+the+dude/
 * Description: A set of JS utilities for WP development
-* Version: 1.0.2
+* Version: 1.0.3
 * Author: Nimrod Cohen
 * Author URI: https://google.com/?q=who+is+the+dude
 * Tested up to: 5.8.2

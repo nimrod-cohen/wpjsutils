7c24e542fd94f4ab64e3402b1879da8b4896c1dc
nimrod-cohen <nimrod.cohen@gmail.com>
2023-12-12 11:04:33 +0200

    z-index of notifications

--- wpjsutils.php
+++ wpjsutils.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPJSUtils
  * Plugin URI: https://google.com?q=who+is+the+dude/
  * Description: A set of JS utilities for WP development
- * Version: 1.3.2
+ * Version: 1.3.3
  * Author: Nimrod Cohen
  * Author URI: https://google.com/?q=who+is+the+dude
  * Tested up to: 6.4.1

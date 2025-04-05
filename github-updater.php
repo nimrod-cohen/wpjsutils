2bdfe523fb5801a6177d2d05aa8edfb8628cb6f6
nimrod-cohen <nimrod.cohen@gmail.com>
2025-04-05 18:04:37 +0300

    moved to nested css + git updater

---
+++ github-updater.php
@@ -0,0 +1,317 @@
+<?php
+/*
+ * Plugin name: Misha Update Checker
+ * Description: This simple plugin does nothing, only gets updates from a custom server
+ * Version: 1.4
+ * Author: Misha Rudrastyh, changes by Nimrod Cohen, Oleg Shumar, Milen Petrov
+ * Author URI: https://rudrastyh.com
+ * License: GPL
+ *
+ * Make sure to set Author to your github user handle and Version in the plugin header
+ * use the .git/hooks/pre-commit and post-commit to automatically update the version number
+ * in readme.md files if necessary (there's a pre-commit.sample file)
+ * and create a tag with the version number
+ */
+
+/**/
+namespace WPJSUtils;
+
+defined('ABSPATH') || exit;
+
+class GitHubPluginUpdater {
+  /**
+   * This is the naming of the folder
+   */
+  private $plugin_slug;
+  private $latest_release_cache_key;
+  private $cache_allowed;
+  private $latest_release = null;
+  private $plugin_file = null;
+  private $plugin_data = null;
+  private $_masterTempFolderName = 'temp_folder';
+  private $_masterTempFolderPath = null;
+  private $_pluginTempFolderPath = null;
+
+  private function get_plugin_data() {
+    if ($this->plugin_data) {
+      return;
+    }
+    $this->plugin_data = get_plugin_data(WP_PLUGIN_DIR . '/' . $this->plugin_file);
+
+  }
+
+  public function __construct($base_file) {
+    $this->plugin_file = str_replace(WP_PLUGIN_DIR . '/', '', $base_file);
+    $this->plugin_slug = explode('/', plugin_basename($base_file))[0];
+    $this->latest_release_cache_key = $this->plugin_slug . '_release';
+    $this->cache_allowed = true;
+    $this->get_plugin_data();
+
+    // Make sure we have master temp folder available
+    $uploads_dir = wp_upload_dir();
+    $this->_masterTempFolderPath = $uploads_dir['basedir'] . '/' . $this->_masterTempFolderName;
+    if (!is_dir($this->_masterTempFolderPath)) {
+      mkdir($this->_masterTempFolderPath, 0755, true);
+    }
+
+    // Make sure we have plugin temp folder available
+    $this->_pluginTempFolderPath = $this->_masterTempFolderName . '/' . $this->plugin_slug . '_tmp';
+    if (!is_dir($this->_pluginTempFolderPath)) {
+      mkdir($this->_pluginTempFolderPath, 0755, true);
+    }
+
+    add_filter('plugins_api', [$this, 'get_plugin_info'], 20, 3);
+    add_filter('site_transient_update_plugins', [$this, 'update']);
+    add_action('upgrader_process_complete', [$this, 'finish_install'], 10, 2);
+    add_action('upgrader_post_install', [$this, 'fix_folder'], 10, 3);
+
+    add_action('admin_post_' . $this->plugin_slug . '_clear_cache', [$this, 'clear_latest_release_cache']);
+    add_action('admin_notices', [$this, 'display_cache_cleared_message']);
+    add_filter('plugin_action_links_' . $this->plugin_file, [$this, 'add_clear_cache_link']);
+  }
+
+  public function add_clear_cache_link($links) {
+    $url = admin_url('admin-post.php?action=' . $this->plugin_slug . '_clear_cache');
+    $link = "<a href='$url'>Clear Cache</a>";
+    return array_merge($links, [$link]);
+  }
+
+  public function clear_latest_release_cache() {
+    if ($this->cache_allowed) {
+      delete_transient($this->latest_release_cache_key);
+    }
+    delete_site_transient('update_plugins');
+    wp_update_plugins();
+
+    wp_redirect(add_query_arg('cache_cleared_' . $this->plugin_slug, 'true', wp_get_referer()));
+    exit;
+  }
+
+  public function display_cache_cleared_message() {
+    if (!isset($_GET['cache_cleared_' . $this->plugin_slug])) {
+      return;
+    }
+    echo "<div class='notice notice-success is-dismissible'>
+        <p>Cache cleared successfully</p>
+      </div>
+      ";
+  }
+
+  function get_plugin_info($res, $action, $args) {
+    // do nothing if you're not getting plugin information right now
+    if ('plugin_information' !== $action || $this->plugin_slug !== $args->slug) {
+      return $res;
+    }
+    if (!$this->get_latest_release() || !$this->latest_release) {
+      return $res;
+    }
+
+    $plugin = [
+      'name' => $this->plugin_data['Name'],
+      'slug' => $this->plugin_slug,
+      'requires' => $this->plugin_data['RequiresWP'],
+      'tested' => $this->plugin_data['TestedUpTo'] ?? null,
+      'version' => $this->latest_release['tag_name'],
+      'author' => $this->plugin_data['AuthorName'],
+      'author_profile' => $this->plugin_data['AuthorURI'],
+      'last_updated' => $this->latest_release['published_at'],
+      'homepage' => $this->plugin_data['PluginURI'],
+      'short_description' => $this->plugin_data['Description'],
+      'sections' => [
+        'Description' => $this->plugin_data['Description'],
+        'Updates' => $this->latest_release['body']
+      ],
+      'download_link' => $this->latest_release['zipball_url']
+    ];
+
+    return (object) $plugin;
+  }
+
+  private function get_latest_release() {
+    if ($this->latest_release) {
+      return true;
+    }
+
+    $transient = null;
+    if ($this->cache_allowed) {
+      $transient = get_transient($this->latest_release_cache_key);
+    }
+
+    if ($transient) {
+      $this->latest_release = $transient;
+      return true;
+    }
+
+    $github_api_url = 'https://api.github.com/repos/' . $this->plugin_data['AuthorName'] . '/' . $this->plugin_slug . '/releases/latest';
+
+    // Make the API request to GitHub
+    $response = wp_remote_get($github_api_url);
+    if (is_wp_error($response)) {
+      return false;
+    }
+
+    $this->latest_release = json_decode(wp_remote_retrieve_body($response), true);
+    $this->latest_release["version"] = preg_replace('/[^0-9.]/', '', $this->latest_release["tag_name"]);
+
+    if ($this->cache_allowed) {
+      set_transient($this->latest_release_cache_key, $this->latest_release, 5 * MINUTE_IN_SECONDS);
+    }
+
+    return true;
+  }
+
+  public function update($transient) {
+
+    if (empty($transient->checked)) {
+      return $transient;
+    }
+
+    // GitHub API URL for the latest release
+    if (!$this->get_latest_release() || !$this->latest_release) {
+      return $transient;
+    }
+
+    if (
+      version_compare($this->plugin_data["Version"], $this->latest_release["version"], '<')) {
+      $res = new stdClass();
+      $res->slug = $this->plugin_slug;
+      $res->plugin = $this->plugin_file; // misha-update-plugin/misha-update-plugin.php
+      $res->new_version = $this->latest_release["version"];
+      $res->tested = $this->plugin_data["TestedUpTo"] ?? null;
+
+      $res->package = $this->process_zip_file($this->latest_release["zipball_url"]);
+
+      $transient->response[$res->plugin] = $res;
+    }
+
+    return $transient;
+  }
+
+  public function finish_install($upgrader, $options) {
+    if (
+      'update' !== $options['action']
+      || 'plugin' === $options['type']
+    ) {
+      return;
+    }
+
+    // just clean the cache when new plugin version is installed
+    if ($this->cache_allowed) {
+      delete_transient($this->latest_release_cache_key);
+    }
+  }
+
+  public function fix_folder($response, $hook_extra, $result) {
+    $tmp_name = $this->get_tmp_name();
+    if (file_exists($tmp_name)) {
+      unlink($tmp_name);
+    }
+
+    $this->delete_directory($this->_pluginTempFolderPath);
+  }
+
+  /**
+   * Process and manipulate a zip file from a given URL.
+   *
+   * @param string $file_url The URL of the zip file to be processed.
+   *
+   * @return string The path to the newly created zip file after processing, or an error message.
+   */
+  public function process_zip_file($file_url) {
+    require_once ABSPATH . 'wp-admin/includes/file.php';
+
+    $response = wp_remote_get($file_url, array('timeout' => 300));
+    if (is_wp_error($response) || 200 !== wp_remote_retrieve_response_code($response)) {
+      return 'Error downloading the zip file.';
+    }
+
+    $zip_content = $response['body'];
+    $tmp_file = wp_tempnam($file_url);
+    file_put_contents($tmp_file, $zip_content);
+
+    WP_Filesystem();
+
+    $unzipfile = unzip_file($tmp_file, $this->_pluginTempFolderPath);
+    if (is_wp_error($unzipfile)) {
+      unlink($tmp_file);
+      return 'Error unzipping the file.';
+    }
+
+    unlink($tmp_file);
+    $unzipped_dirs = glob($this->_pluginTempFolderPath . '/*', GLOB_ONLYDIR);
+    if (empty($unzipped_dirs)) {
+      return 'Error: No directory found after unzipping.';
+    }
+
+    $unzipped_dir = $unzipped_dirs[0];
+    $new_name = $this->_pluginTempFolderPath . '/' . $this->plugin_slug;
+
+    if (!$this->safe_rename($unzipped_dir, $new_name)) {
+      return 'Error renaming the directory.';
+    }
+
+    $zip = new ZipArchive();
+    $new_zip_name = $this->get_tmp_name();
+
+    if ($zip->open($new_zip_name, ZipArchive::CREATE) === true) {
+      $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($new_name, RecursiveDirectoryIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST);
+      foreach ($iterator as $filename => $fileobject) {
+        $local_path = $this->plugin_slug . '/' . substr($filename, strlen($new_name) + 1);
+        if (!$fileobject->isDir()) {
+          $zip->addFile($filename, $local_path);
+        }
+      }
+      $zip->close();
+    } else {
+      return 'Error creating the .zip file.';
+    }
+
+    return $new_zip_name;
+  }
+
+  private function safe_rename($source, $destination, $retries = 5) {
+    // Make sure destination does not exist, so it will not throw errors
+    $this->delete_directory($destination);
+
+    clearstatcache();
+    for ($i = 0; $i < $retries; $i++) {
+      if (rename($source, $destination)) {
+        return true;
+      }
+      clearstatcache();
+    }
+    return false;
+  }
+
+  /**
+   * Retrieves a temporary file name for a specific directory.
+   *
+   * @return string The temporary file name including the directory path.
+   */
+  private function get_tmp_name() {
+    $uploads_dir = wp_upload_dir();
+
+    return $uploads_dir['basedir'] . '/' . $this->plugin_slug . ".zip";
+  }
+
+  /**
+   * Clean-up. Deletes a directory and its contents recursively.
+   *
+   * @param string $dir The directory path to be deleted.
+   *
+   * @return bool True if the directory and its contents are successfully deleted, false otherwise.
+   */
+  private function delete_directory($dir) {
+    if (!is_dir($dir)) {
+      return false;
+    }
+
+    $files = array_diff(scandir($dir), ['.', '..']);
+    foreach ($files as $file) {
+      $path = "$dir/$file";
+      is_dir($path) ? $this->delete_directory($path) : unlink($path);
+    }
+
+    return rmdir($dir);
+  }
+}
--- wpjsutils.php
+++ wpjsutils.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPJSUtils
  * Plugin URI: https://google.com?q=who+is+the+dude/
  * Description: A set of JS utilities for WP development
- * Version: 1.5.16
+ * Version: 1.6.0
  * Author: Nimrod Cohen
  * Author URI: https://google.com/?q=who+is+the+dude
  * Tested up to: 6.7.1
@@ -30,135 +30,142 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-class WPJSUtils {
-  function __construct() {
-    add_action('init', [$this, 'init']);
+if (!class_exists('WPJSUtils')) {
 
-    add_action('wp_ajax_nopriv_check_email', [$this, 'checkEmailAddress']);
-    add_action('wp_ajax_check_email', [$this, 'checkEmailAddress']);
-  }
+  class WPJSUtils {
+    function __construct() {
+      add_action('init', [$this, 'init']);
 
-  function init() {
-    if (!function_exists('get_plugin_data')) { //allowing plugin data to be available for frontend calls
-      require_once ABSPATH . 'wp-admin/includes/plugin.php';
+      add_action('wp_ajax_nopriv_check_email', [$this, 'checkEmailAddress']);
+      add_action('wp_ajax_check_email', [$this, 'checkEmailAddress']);
+
+      add_action('admin_init', function () {
+        $updater = new \WPJSUtils\GitHubPluginUpdater(__FILE__);
+      });
     }
 
-    $plugin_data = get_plugin_data(__FILE__);
-    $plugin_version = $plugin_data['Version'];
-    $cachebust = "?time=" . date('Y_m_d_H') . "&v=" . $plugin_version;
+    function init() {
+      if (!function_exists('get_plugin_data')) { //allowing plugin data to be available for frontend calls
+        require_once ABSPATH . 'wp-admin/includes/plugin.php';
+      }
 
-    wp_enqueue_script('wpjsutils', plugin_dir_url(__FILE__) . 'js/jsutils.js' . $cachebust);
-    wp_enqueue_script('emailvalidator-js', plugin_dir_url(__FILE__) . 'js/emailvalidator.js' . $cachebust, ["wpjsutils"]);
-    wp_enqueue_script('remodaler-js', plugin_dir_url(__FILE__) . 'js/remodaler.js' . $cachebust, ["wpjsutils"]);
-    wp_enqueue_style('remodaler-css', plugin_dir_url(__FILE__) . 'css/remodaler.css' . $cachebust);
-    wp_enqueue_script('popover-js', plugin_dir_url(__FILE__) . 'js/popover.js' . $cachebust, ["wpjsutils"]);
-    wp_enqueue_style('popover-css', plugin_dir_url(__FILE__) . 'css/popover.css' . $cachebust);
-    wp_enqueue_script('notifications-js', plugin_dir_url(__FILE__) . 'js/notifications.js' . $cachebust, ["wpjsutils"]);
-    wp_enqueue_style('notifications-css', plugin_dir_url(__FILE__) . 'css/notifications.css' . $cachebust);
-    wp_enqueue_script('infinity-js', plugin_dir_url(__FILE__) . 'js/infinityscroll.js' . $cachebust, ["wpjsutils"]);
-    wp_enqueue_script('monthpicker-js', plugin_dir_url(__FILE__) . 'js/monthpicker.js' . $cachebust, ["wpjsutils"]);
-    wp_enqueue_style('monthpicker-css', plugin_dir_url(__FILE__) . 'css/monthpicker.css' . $cachebust);
-    wp_enqueue_script('switch-js', plugin_dir_url(__FILE__) . 'js/switch.js' . $cachebust, ["wpjsutils"]);
-    wp_enqueue_style('switch-css', plugin_dir_url(__FILE__) . 'css/switch.css' . $cachebust);
-    wp_enqueue_script('dropdown-js', plugin_dir_url(__FILE__) . 'js/dropdown.js' . $cachebust, ["wpjsutils"]);
-    wp_enqueue_style('dropdown-css', plugin_dir_url(__FILE__) . 'css/dropdown.css' . $cachebust);
-    wp_enqueue_script('tagcloud-js', plugin_dir_url(__FILE__) . 'js/tagcloud.js' . $cachebust, ["wpjsutils"]);
-    wp_enqueue_style('tagcloud-css', plugin_dir_url(__FILE__) . 'css/tagcloud.css' . $cachebust);
-    wp_enqueue_script('tabs-js', plugin_dir_url(__FILE__) . 'js/tabs.js' . $cachebust, ["wpjsutils"]);
-    wp_enqueue_style('tabs-css', plugin_dir_url(__FILE__) . 'css/tabs.css' . $cachebust);
+      $plugin_data = get_plugin_data(__FILE__);
+      $plugin_version = $plugin_data['Version'];
+      $cachebust = "?time=" . date('Y_m_d_H') . "&v=" . $plugin_version;
 
-    wp_localize_script('wpjsutils',
-      'wpjsutils_data', [
-        'ajax_url' => admin_url('admin-ajax.php')
-      ]);
-  }
+      wp_enqueue_script('wpjsutils', plugin_dir_url(__FILE__) . 'js/jsutils.js' . $cachebust);
+      wp_enqueue_script('emailvalidator-js', plugin_dir_url(__FILE__) . 'js/emailvalidator.js' . $cachebust, ["wpjsutils"]);
+      wp_enqueue_script('remodaler-js', plugin_dir_url(__FILE__) . 'js/remodaler.js' . $cachebust, ["wpjsutils"]);
+      wp_enqueue_style('remodaler-css', plugin_dir_url(__FILE__) . 'css/remodaler.css' . $cachebust);
+      wp_enqueue_script('popover-js', plugin_dir_url(__FILE__) . 'js/popover.js' . $cachebust, ["wpjsutils"]);
+      wp_enqueue_style('popover-css', plugin_dir_url(__FILE__) . 'css/popover.css' . $cachebust);
+      wp_enqueue_script('notifications-js', plugin_dir_url(__FILE__) . 'js/notifications.js' . $cachebust, ["wpjsutils"]);
+      wp_enqueue_style('notifications-css', plugin_dir_url(__FILE__) . 'css/notifications.css' . $cachebust);
+      wp_enqueue_script('infinity-js', plugin_dir_url(__FILE__) . 'js/infinityscroll.js' . $cachebust, ["wpjsutils"]);
+      wp_enqueue_script('monthpicker-js', plugin_dir_url(__FILE__) . 'js/monthpicker.js' . $cachebust, ["wpjsutils"]);
+      wp_enqueue_style('monthpicker-css', plugin_dir_url(__FILE__) . 'css/monthpicker.css' . $cachebust);
+      wp_enqueue_script('switch-js', plugin_dir_url(__FILE__) . 'js/switch.js' . $cachebust, ["wpjsutils"]);
+      wp_enqueue_style('switch-css', plugin_dir_url(__FILE__) . 'css/switch.css' . $cachebust);
+      wp_enqueue_script('dropdown-js', plugin_dir_url(__FILE__) . 'js/dropdown.js' . $cachebust, ["wpjsutils"]);
+      wp_enqueue_style('dropdown-css', plugin_dir_url(__FILE__) . 'css/dropdown.css' . $cachebust);
+      wp_enqueue_script('tagcloud-js', plugin_dir_url(__FILE__) . 'js/tagcloud.js' . $cachebust, ["wpjsutils"]);
+      wp_enqueue_style('tagcloud-css', plugin_dir_url(__FILE__) . 'css/tagcloud.css' . $cachebust);
+      wp_enqueue_script('tabs-js', plugin_dir_url(__FILE__) . 'js/tabs.js' . $cachebust, ["wpjsutils"]);
+      wp_enqueue_style('tabs-css', plugin_dir_url(__FILE__) . 'css/tabs.css' . $cachebust);
 
-  /*
-  function checkEmailSMTP($email) {
-  $connect = null;
-  $result = true;
+      wp_localize_script('wpjsutils',
+        'wpjsutils_data', [
+          'ajax_url' => admin_url('admin-ajax.php')
+        ]);
+    }
 
-  try {
-  //TODO: have the email address set in settings
-  $from = "yinon@bursa4u.com";
-  list($user, $domain) = explode('@', $email);
-  list($fromUser, $fromDomain) = explode('@', $from);
+    /*
+    function checkEmailSMTP($email) {
+    $connect = null;
+    $result = true;
 
-  getmxrr($domain, $mxhosts, $mxweight);
-
-  if(empty($mxhosts) ) throw new Exception('Domain without MX records');
-
-  $mxIP = $mxhosts[array_search(min($mxweight), $mxweight)];
-
-  $connect = @fsockopen($mxIP, 25);
-
-  if(!$connect) throw new Exception('Cannot connect to domain');
-
-  $response = fgets($connect);
-  if(!preg_match("/^220/i", $response)) throw new Exception('Domain refused connection');
-
-  fputs($connect , "EHLO ".$fromDomain."\r\n");
-  $response = fgets($connect);
-
-  fputs($connect , "MAIL FROM: <".$from.">\r\n");
-  $response = fgets($connect);
-
-  fputs($connect , "RCPT TO: <".$email.">\r\n");
-  $response = fgets($connect);
-  if(!preg_match("/^250/i", $response)) throw new Exception('Email address refused by server');
-
-  } catch(Exception $ex) {
-  $result = false;
-  }
-
-  if($connect) {
-  fputs ($connect , "QUIT");
-  fclose($connect);
-  }
-
-  return $result;
-  }
-   */
-
-  public static function validateEmailAddress($email) {
     try {
-      //striping the +, because FILTER_VALIDATE_EMAIL does not like it.
-      $email = (strpos($email, '+') !== false) ? preg_replace('/\+.*@/', '@', $email) : $email;
+    //TODO: have the email address set in settings
+    $from = "yinon@bursa4u.com";
+    list($user, $domain) = explode('@', $email);
+    list($fromUser, $fromDomain) = explode('@', $from);
 
-      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        return false;
-      }
+    getmxrr($domain, $mxhosts, $mxweight);
 
-      list($user, $domain) = explode('@', $email);
+    if(empty($mxhosts) ) throw new Exception('Domain without MX records');
 
-      if ($domain == 'gmail.com' && strlen($user) <= 4) {
-        return false;
-      }
+    $mxIP = $mxhosts[array_search(min($mxweight), $mxweight)];
 
-      $dig = shell_exec("dig MX " . $domain . " +short");
+    $connect = @fsockopen($mxIP, 25);
 
-      if (strlen($dig) == 0) {
-        return false;
-      }
+    if(!$connect) throw new Exception('Cannot connect to domain');
 
-      //this returns a bogus mx203.inbound-mx.org and mx203.inbound-mx.net records on the server.
-      //if(!checkdnsrr($domain, 'MX')) throw new Exception('Domain without MX records');
+    $response = fgets($connect);
+    if(!preg_match("/^220/i", $response)) throw new Exception('Domain refused connection');
 
-      //this check is too dangerous - can hang and can just take too much time.
-      // if(!strstr($domain, 'yahoo') && !strstr($domain, 'hotmail')) {
-      //    if(!$this->checkEmailSMTP($email)) throw new Exception('Failed to connect with SMTP');
-      // }
-    } catch (Exception $ex) {
-      return false;
+    fputs($connect , "EHLO ".$fromDomain."\r\n");
+    $response = fgets($connect);
+
+    fputs($connect , "MAIL FROM: <".$from.">\r\n");
+    $response = fgets($connect);
+
+    fputs($connect , "RCPT TO: <".$email.">\r\n");
+    $response = fgets($connect);
+    if(!preg_match("/^250/i", $response)) throw new Exception('Email address refused by server');
+
+    } catch(Exception $ex) {
+    $result = false;
     }
-    return true;
+
+    if($connect) {
+    fputs ($connect , "QUIT");
+    fclose($connect);
+    }
+
+    return $result;
+    }
+     */
+
+    public static function validateEmailAddress($email) {
+      try {
+        //striping the +, because FILTER_VALIDATE_EMAIL does not like it.
+        $email = (strpos($email, '+') !== false) ? preg_replace('/\+.*@/', '@', $email) : $email;
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          return false;
+        }
+
+        list($user, $domain) = explode('@', $email);
+
+        if ($domain == 'gmail.com' && strlen($user) <= 4) {
+          return false;
+        }
+
+        $dig = shell_exec("dig MX " . $domain . " +short");
+
+        if (strlen($dig) == 0) {
+          return false;
+        }
+
+        //this returns a bogus mx203.inbound-mx.org and mx203.inbound-mx.net records on the server.
+        //if(!checkdnsrr($domain, 'MX')) throw new Exception('Domain without MX records');
+
+        //this check is too dangerous - can hang and can just take too much time.
+        // if(!strstr($domain, 'yahoo') && !strstr($domain, 'hotmail')) {
+        //    if(!$this->checkEmailSMTP($email)) throw new Exception('Failed to connect with SMTP');
+        // }
+      } catch (Exception $ex) {
+        return false;
+      }
+      return true;
+    }
+
+    function checkEmailAddress() {
+      $result = ["success" => WPJSUtils::validateEmailAddress($_POST['email'])];
+      echo json_encode($result);
+      die;
+    }
   }
 
-  function checkEmailAddress() {
-    $result = ["success" => WPJSUtils::validateEmailAddress($_POST['email'])];
-    echo json_encode($result);
-    die;
-  }
+  $wpjsu = new WPJSUtils();
 }
-
-$wpjsu = new WPJSUtils();
